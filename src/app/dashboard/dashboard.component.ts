ce5164187d74801a765e8b7594d3eb6a76c52653
thiagoabreu72@gmail.com <thiagoabreu72@gmail.com>
2023-02-19 22:50:36 -0300

    Inserido gráfico para controle de percentual de metas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,33 +1,68 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnChanges {
-
+export class DashboardComponent implements OnChanges, OnInit {
   @Input() dadosInsercao: any = {};
   dados: any;
   dadosStorage: any[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnChanges(alteracoes: SimpleChanges): void {
-    if ((alteracoes['dadosInsercao'].currentValue !== alteracoes['dadosInsercao'].previousValue)
-      && alteracoes['dadosInsercao'].firstChange !== true) {
-
+    if (
+      alteracoes['dadosInsercao'].currentValue !==
+        alteracoes['dadosInsercao'].previousValue &&
+      alteracoes['dadosInsercao'].firstChange !== true
+    ) {
       this.gravarDados();
     }
   }
 
+  ngOnInit() {
+    const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      const myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [
+            {
+              data: [66, 34],
+              backgroundColor: ['#6FED5C', '#FA7462'],
+            },
+          ],
+        },
+        options: {
+          responsive: false,
+          plugins: {
+            title: { text: 'Meta do Mês em %', display: true },
+          },
+        },
+      });
+    } else {
+      alert('Não há valores para geração do gráfico.');
+      // Tratar o caso em que ctx é nulo (por exemplo, mostrar uma mensagem de erro)
+    }
+  }
+
   lerDadosJson() {
-    this.http.get('./assets/dados.json').subscribe(dados => {
+    this.http.get('./assets/dados.json').subscribe((dados) => {
       this.dados = dados;
       console.log(this.dados);
-    })
+    });
   }
 
   gravarDados() {
@@ -45,24 +80,20 @@ export class DashboardComponent implements OnChanges {
       observacao: "testando123456"
     }]*/
 
-
     let retorno = this.consultarDados() || [];
 
     for (let i = 0; i < retorno.length; i++) {
-
-      console.log(retorno[i])
+      console.log(retorno[i]);
       dadosLocais.push(retorno[i]);
     }
 
     dadosLocais.push(this.dadosInsercao);
     localStorage.setItem('dados', JSON.stringify(dadosLocais));
-
   }
 
   consultarDados() {
     //console.log(JSON.parse(localStorage.getItem('dados') || ''))
-    let objeto = JSON.parse(localStorage.getItem('dados') || '0')
+    let objeto = JSON.parse(localStorage.getItem('dados') || '0');
     return objeto;
   }
-
 }
